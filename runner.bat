510397338f13096c3219d13c35d0c118fa1fc206
adcerro <adcerro@uninorte.edu.co>
2025-05-29 16:33:10 -0500

    new runner

--- runner.bat
+++ runner.bat
@@ -1,3 +1,17 @@
-powershell.exe -executionpolicy unrestricted %~dp0\script.ps1
+@echo off
+:: Check for admin rights
+net session >nul 2>&1
+if %errorlevel%==0 (
+    echo Running with admin rights...
+) else (
+    echo Requesting admin privileges...
+    powershell -Command "Start-Process '%~f0' -Verb runAs"
+    exit /b
+)
+
+:: Your real commands go below this line
+powershell.exe -ExecutionPolicy Bypass -File "%dp0\script.ps1"
+
+PAUSE
 
 PAUSE
